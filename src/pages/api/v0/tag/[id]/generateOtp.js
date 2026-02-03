77f3095c68b93fbc68248fa053050e11c7e3eb62
Rahul Raj <rahul.raj@gohighlevel.com>
2026-02-04 00:20:11 +0530

    feat: add generate otp api

--- src/pages/api/v0/tag/[id]/generateOtp.js
+++ src/pages/api/v0/tag/[id]/generateOtp.js
@@ -12,6 +12,7 @@ class GenerateOtpController extends PublicController {
    * Save OTP in Tag.otp.code, expires_at, attempts, last_attempt_at
    * Send OTP to phone number
    * Return success or error
+   * Rate limiting: same IP, max 1 attempt per 1 minute
    */
   async post(req, res) {
     const tagId = req?.params?.id;
