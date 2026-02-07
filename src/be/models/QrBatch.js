dae4524041dc7f51e165b28eb896e59628d7cdea
srivastavarahul442 <srivastavarahul442@gmail.com>
2026-02-07 16:16:24 +0530

    updated the tag type & related apis

--- src/be/models/QrBatch.js
+++ src/be/models/QrBatch.js
@@ -1,12 +1,17 @@
 import { Schema, model, models, Types } from "mongoose";
 
+const batchType = [
+  "car","bike","bag-tag","door-tag","business-card"
+]
+
 const QrBatchSchema = new Schema(
   {
     batch_id: { type: String, required: true, unique: true, index: true }, // BATCH001
     qty: { type: Number, required: true, min: 1 }, // 100
     //created_by: { type: Types.ObjectId, ref: "User", required: true },
-
+    type:{type:String,enum:batchType,required:true},
     note: { type: String, trim: true },
+    isTestBatch:{type:Boolean,default:false}, // false = real batch, true = testing batch
     status: { type: String, enum: ["new","generating","created", "in-printing", "distributed"], default: "created" },
   },
   { timestamps: true }
